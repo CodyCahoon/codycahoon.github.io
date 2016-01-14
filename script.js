eb49bc9cb4efefc6f22014068c7e558beba79476
Cody Cahoon <codycahoon@Codys-MacBook-Pro.local>
2016-01-14 16:26:28 -0500

    init

---
+++ script.js
@@ -0,0 +1,55 @@
+$("#nav-list li:nth-child(5)").click(function(){
+    toggle(0);
+});
+
+$("#nav-list li:nth-child(6)").click(function(){
+    toggle(1);
+});
+
+$("#nav-list li:nth-child(7)").click(function(){
+    toggle(2);
+});
+
+$("#nav-list li:nth-child(8)").click(function(){
+    toggle(3);
+});
+
+function toggle(mode)
+{
+    $(".nav-item").removeClass("selected-nav");
+    switch (mode)
+    {
+        case 0:
+            $(".about-grid").addClass("hidden-top");
+            $(".project-grid").removeClass("hidden-right");
+            $("#nav-list li:nth-child(5)").addClass("selected-nav");
+        break;
+
+        case 1:
+            $(".about-grid").removeClass("hidden-top");
+            $(".project-grid").addClass("hidden-right");
+            $("#nav-list li:nth-child(6)").addClass("selected-nav");
+
+        break;
+
+
+        case 2:
+            $("#nav-list li:nth-child(7)").addClass("selected-nav");
+
+
+        break;
+
+        default:
+            $("#nav-list li:nth-child(8)").addClass("selected-nav");
+
+        break;
+    }
+}
+
+$(document).mousemove(function(event) {
+    var currentMousePos = { x: -1, y: -1 };
+
+    currentMousePos.x = event.pageX;
+    currentMousePos.y = event.pageY;
+//    console.log(currentMousePos);
+});
