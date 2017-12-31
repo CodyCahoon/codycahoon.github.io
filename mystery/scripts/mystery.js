95b250060d522c05ae286c2cd3a15b9620d3c6ea
Cody Cahoon <codyjcahoon@gmail.com>
2017-12-31 11:34:11 -0500

    Added clue styles

---
+++ mystery/scripts/mystery.js
@@ -0,0 +1,13 @@
+
+
+function toggleClue(clueNumber) {
+    var clue = document.getElementById('clue-' + clueNumber);
+    if (clue) {
+        var isHidden = clue.classList.contains('hidden');
+        if (isHidden) {
+            clue.className = 'open-clue';
+        } else {
+            clue.className = 'open-clue hidden';
+        }
+    }
+}
